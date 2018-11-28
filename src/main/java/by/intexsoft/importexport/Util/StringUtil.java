f3dd87c965fa48a3192c3b2e89bb534aa878a406
sergey.buzuk <sergey.buzuk@intexsoft.by>
2018-11-28 13:43:00 +0300

    refactor string util

--- src/main/java/by/intexsoft/importexport/Util/StringUtil.java
+++ src/main/java/by/intexsoft/importexport/Util/StringUtil.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 public final class StringUtil {
     public static String getStringType(String path){
-        String name = new File(path).getName().split("_")[0];
-        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+        return new File(path).getName().split("_")[0];
     }
 }
